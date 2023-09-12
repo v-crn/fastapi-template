7dfebf46f8c6fd1948b8498331add6f779c49435
v-crn <var.cyrano@gmail.com>
2023-09-12 23:36:59 +0900

    add make commands for docker

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -3,11 +3,25 @@ bash:
 	docker-compose exec ${SERVICE_NAME} /bin/bash
 
 
-.PHONY: clear
-clear:
-	docker-compose down --rmi all --volumes
-
-
 .PHONY: scan_image
 scan_image:
 	docker scan --json ${IMAGE_NAME} --severity=high > docker-scan-result.json
+
+
+.PHONY: tag
+tag:
+	docker tag ${IMAGE_ID} ${DOCKER_HUB_USERNAME}/${IMAGE_NAME}:${TAG}
+
+
+.PHONY: push_image
+push_image: tag
+	docker push ${DOCKER_HUB_USERNAME}/${IMAGE_NAME}:${TAG}
+
+
+.PHONY: remove_container
+remove_container:
+	docker-compose rm
+
+
+remove_all_images:
+	docker-compose down --rmi all --volumes --remove-orphans
